1cad111103503e08686e0217e6b947892c571861
emelkonyan <edimelkonyan@gmail.com>
2016-04-19 11:55:34 +0300

    Fixes & README.md

--- config/epay-wrapper.php
+++ config/epay-wrapper.php
@@ -1,9 +1,17 @@
 <?php defined('SYSPATH') or die('No direct access allowed!');
 
+
+
 return array(
 
+
         'api_base' => 'https://demo.epay.bg/xdev/api/',
+
         'appid' => 'MyApp',
+
         'secret' => 'SomeSecretWord',
+
         'deviceid' => 'TehDeviceId'
+
+
 );
